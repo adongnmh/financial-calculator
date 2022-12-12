4880131f34edb2506baf19b4f0c31ebd7869bdec
Andrew Dong <adongnmh@gmail.com>
2022-12-11 22:31:25 -0700

    Setting up the Payload Structure
    - Setting up payload structure

--- app/internal/mortgage/MortgagePayload.go
+++ app/internal/mortgage/MortgagePayload.go
@@ -2,28 +2,46 @@ package mortgage
 
 import (
 	"encoding/json"
+	"github.com/go-playground/validator/v10"
 	"io"
 )
 
 const (
-	BiWeekly      string = "bi-weekly"
-	BiWeeklyAccel        = "bi-weekly-accelerated"
-	Monthly              = "monthly"
+	BiWeekly      = "bi-weekly"
+	BiWeeklyAccel = "bi-weekly-accelerated"
+	Monthly       = "monthly"
 )
 
-type Payload struct {
-	PropertyPrice      float64 `json:"property_price"`
-	DownPayment        float64 `json:"down_payment"`
-	AnnualInterestRate float64 `json:"annual_interest_rate"`
-	Period             int64   `json:"period"`
-	PaymentSchedule    string  `json:"payment_schedule"`
+type PayloadMortgage struct {
+	PropertyPrice      float64 `json:"property_price" validate:"gte=1"`
+	DownPayment        float64 `json:"down_payment" validate:"gte=0"`
+	AnnualInterestRate float64 `json:"annual_interest_rate" validate:"gte=0"`
+	Period             int64   `json:"period" validate:"required"`
+	PaymentSchedule    string  `json:"payment_schedule" validate:"required,schedule"`
 }
 
-func (mr *Payload) decodePayload(r io.Reader) error {
+func (mr *PayloadMortgage) decodePayload(r io.Reader) error {
 	decoder := json.NewDecoder(r)
 	err := decoder.Decode(mr)
 	if err != nil {
 		return err
 	}
 	return nil
+}
+
+func (mr *PayloadMortgage) validateFields(payload *PayloadMortgage) error {
+	validate := validator.New()
+	err := validate.RegisterValidation("schedule", validatePaymentSchedule)
+	if err != nil {
+		return err
+	}
+	return validate.Struct(payload)
+}
+
+func validatePaymentSchedule(fl validator.FieldLevel) bool {
+	switch fl.Field().String() {
+	case BiWeekly, BiWeeklyAccel, Monthly:
+		return true
+	}
+	return false
 }
