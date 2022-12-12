ec21ea88a47768d1f56dcf7ce3781e55ade5f57b
Andrew Dong <adongnmh@gmail.com>
2022-12-11 23:38:03 -0700

    Adding Server.go + Handlers
    - Basic server to call mortgage handlers
    - Mortgage handlers added

--- app/cmd/server.go
+++ app/cmd/server.go
@@ -1,5 +1,32 @@
-package main
+package server
 
-func main() {
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"quoter/internal/mortgage"
+	"quoter/util"
+)
 
+var router *mux.Router
+
+func statusHandler(w http.ResponseWriter, r *http.Request) {
+	util.RespondWithJSON(w, http.StatusOK, map[string]string{"result": "success"})
+}
+
+func initHandlers() {
+	statusRouter := router.Methods(http.MethodGet).Subrouter()
+	statusRouter.HandleFunc("/api/status", statusHandler)
+
+	mortgageRouter := router.Methods(http.MethodPost).Subrouter()
+	mortgageRouter.HandleFunc("/api/calculate-mortgage-payments", mortgage.CalculateMortgagePayments).Methods("POST")
+	mortgageRouter.Use(mortgage.ValidatePayload)
+}
+
+func Start() {
+	router = mux.NewRouter()
+	initHandlers()
+	fmt.Printf("router initialized and listening on 3000\n")
+	log.Fatal(http.ListenAndServe(":3000", router))
 }
--- app/internal/mortgage/mortgageHandler.go
+++ app/internal/mortgage/mortgageHandler.go
@@ -1,5 +1,13 @@
-package main
+package mortgage
 
-func main() {
+import (
+	"net/http"
+	"quoter/util"
+)
 
+// CalculateMortgagePayments will calculate the mortgage payments with CMHC insurance
+func CalculateMortgagePayments(w http.ResponseWriter, r *http.Request) {
+	payload := r.Context().Value(KeyRequest{}).(*PayloadMortgage)
+	p := CalculateMortgage(payload)
+	util.RespondWithJSON(w, http.StatusOK, map[string]int{"mortgage-payments": p})
 }
